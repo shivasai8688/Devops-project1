584676cc78cb93bee33c7ec0bcb206f9882754f7
shivasai <149556451+shivasai8688@users.noreply.github.com>
2024-04-01 23:07:59 +0530

    Create deploy.sh

---
+++ method2/deploy.sh
@@ -0,0 +1,90 @@
+if [ "$1" == "--help" ]; then
+    echo "Usage: example_script.sh [options] [argument]
+
+Options:
+  -h, --help       Display this help message
+
+Description:
+This script facilitates the installation of microservices on either Docker containers or AWS instances based on the provided parameters.
+"
+exit 0
+fi
+
+#################################################
+
+echo "select the platfoen to get install
+aws
+container
+"
+read platform
+
+echo "select the service to get install
+
+react
+java
+python
+
+"
+read service
+
+git clone https://github.com/TekspotEdu/microserviceapp.git
+
+if [ "$platform" == "container" ]; then
+bash docker_package.sh
+
+case $service in
+
+"react")
+        cd microserviceapp/demo-frontend
+        sudo docker build -t docker_frontendimg . -f Dockerfile
+        sudo docker run docker_frontendimg
+;;
+
+"java")
+        cd microserviceapp/demo-backend1
+        sudo docker build -t docker_backend1img . -f Dockerfile
+        sudo docker run docker_backend1img
+;;
+
+"python")
+        cd microserviceapp/demo-backend2
+        sudo docker build -t docker_backend2img . -f Dockerfile
+        sudo docker run docker_backend2img
+
+;;
+esac
+
+
+else
+
+echo "enter ip address"
+read ip
+case $service in
+  
+"react")
+        ssh ubuntu@$ip "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
+        ssh ubuntu@$ip "sudo apt-get update && sudo apt-get install -y nginx"
+        ssh ubuntu@$ip "sudo service nginx start"
+        ssh ubuntu@$ip "sudo chown -R ubuntu /var/www/html"
+        scp microserviceapp/demo-frontend/build/index.html ubuntu@$ip:/var/www/html/
+
+
+;;
+
+"java")
+        ssh ubuntu@$ip "sudo apt-get update && sudo apt-get install -y openjdk-11-jdk"
+        ssh ubuntu@$ip "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
+        ssh ubuntu@$ip "cd microserviceapp/demo-backend1/target/ && sudo java -jar sentiment-analysis-web-0.0.2-SNAPSHOT.jar --sa.logic.api.url http://$ip:5000"
+
+
+;;
+
+"pyhton")
+        ssh ubuntu@$ip "sudo apt-get update && sudo apt-get install -y python3"
+        ssh ubuntu@$ip "sudo apt-get update && sudo apt-get install -y python3-pip"
+        ssh ubuntu@$ip "sudo git clone "https://github.com/TekspotEdu/microserviceapp.git" "
+        ssh ubuntu@$ip "cd microserviceapp/demo-backend2/sa/ && sudo pip3 install -r requirements.txt && sudo python3 sentiment_analysis.py"
+;;
+
+esac
+fi
